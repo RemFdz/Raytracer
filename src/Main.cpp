c17c352f4dbb364beabf157f0356ccb2bfb84ccc
Romain <romain.collignon@epitech.eu>
2024-05-06 15:33:47 +0200

    refactor(Core): move main to core && use enum keyPressed

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,19 +18,11 @@
 int main(int argc, char **argv)
 {
     Core core(800);
+
     if (!core.validateArguments(argc, argv)) {
         std::cerr << "Invalid arguments. Usage: " << argv[0] << " [-sfml | path_to_config.cfg]" << std::endl;
         return 1;
     }
-
-    // const int width = 800;
-
-    // Rtx::Camera camera(1, 2, Math::Vec3(0, 0, 0), width, Rtx::RenderMode::SFML);
-    // Rtx::Scene scene(camera);
-    // scene.addObject(std::make_shared<Rtx::Sphere>(Math::Vec3(0, 0, -1), 0.5));
-    // scene.addObject(std::make_shared<Rtx::Sphere>(Math::Vec3(0, -100.5, -1), 100));
-
-    // scene.render();
     core.init();
     core.run();
 }
