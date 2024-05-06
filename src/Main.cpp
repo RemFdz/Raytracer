cb112072d52a25640356ffc3763bdccd880e51b0
Pablo ZAPATA <pablo.zapata@epitech.eu>
2024-05-06 16:57:27 +0200

    feat(core): refactoring of the project's main according to the new parsing system

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,6 +8,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
+#include <string>
+#include <cstring>
 
 #include "display/SfmlDisplay.hpp"
 #include "core/Core.hpp"
@@ -18,27 +20,39 @@
 #include "graphics/materials/mirror/Mirror.hpp"
 #include <memory>
 
+#include "parsing/Parsing.hpp"
+
 int main(int argc, char **argv)
 {
-    /*
-    if (!Core::validateArguments(argc, argv)) {
-        std::cerr << "Invalid arguments. Usage: " << argv[0] << " [-sfml | path_to_config.cfg]" << std::endl;
-        return 1;
-    }*/
+    if (argc != 2)
+        return Parsing::showHelp(false);
+    if (!strcmp(argv[1], "--help"))
+        return Parsing::showHelp(false);
 
-    const int width = 800;
+    const std::string configPath(argv[1]);
+    Parsing::Parser parser(configPath);
 
-    Rtx::Camera camera(1, 2, Math::Vec3(0, 0.1, 0.6), width, Rtx::RenderMode::SFML);
+    if (!parser.processFile()) {
+        std::cerr << "Error: Parsing failed" << std::endl;
+        return 84;
+    }
+
+    Rtx::Camera camera(1, 2, parser.getCamCfg().center, parser.getCamCfg().width, Rtx::RenderMode::SFML);
     Rtx::Scene scene(camera);
     Rtx::Material::Lambertian material(Math::Vec3(0.8, 0.3, 0.3));
     Rtx::Material::Mirror material2(Math::Vec3(0.8, 0.6, 0.2));
 
-    scene.addObject(std::make_shared<Rtx::Sphere>(Math::Vec3(0, 0, -1), 0.5, std::make_shared<Rtx::Material::Lambertian>(material)));
-    scene.addObject(std::make_shared<Rtx::Sphere>(Math::Vec3(0, -100.5, -1), 100, std::make_shared<Rtx::Material::Lambertian>(material)));
-    scene.addObject(std::make_shared<Rtx::Sphere>(Math::Vec3(1, 0, -1), 0.5, std::make_shared<Rtx::Material::Mirror>(material2)));
-    scene.addObject(std::make_shared<Rtx::Sphere>(Math::Vec3(-1, 0, -1), 0.5, std::make_shared<Rtx::Material::Mirror>(material2)));
+    // TOOD: Need to be generic, this is for testing purposes
+    for (auto &sphereCfg : parser.getSpheresCfg()) {
+        if (sphereCfg.materialName == "lambertian") {
+            scene.addObject(std::make_shared<Rtx::Sphere>(sphereCfg.center, sphereCfg.radius, std::make_shared<Rtx::Material::Lambertian>(material)));
+        } else if (sphereCfg.materialName == "mirror") {
+            scene.addObject(std::make_shared<Rtx::Sphere>(sphereCfg.center, sphereCfg.radius, std::make_shared<Rtx::Material::Mirror>(material2)));
+        }
+    }
 
     scene.render();
+
     /// Put the call for generate and display in the core class after
     //Core core;
     //core.run(argv[1]);
