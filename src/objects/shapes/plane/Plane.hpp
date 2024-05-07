395ee6c144579f6e4225c4d7e9744c8c507e0e40
Romain <romain.collignon@epitech.eu>
2024-05-07 16:50:23 +0200

    Style(Plane): move function hit to .hpp + clean code

--- src/objects/shapes/plane/Plane.hpp
+++ src/objects/shapes/plane/Plane.hpp
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2024
 ** ray_tracer
 ** File description:
-** Plane implementation
+** Plane
 */
 
 #pragma once
@@ -15,30 +15,15 @@ namespace Rtx {
 
     class Plane : public IObject3D {
     public:
-        // Ajout du paramètre pour le vecteur normal et initialisation dans le constructeur
         Plane(const Math::Point3D& pos, const Math::Vector3D& norm, const std::shared_ptr<IMaterial> &material)
             : position(pos), normal(norm.unitVector()), _material(material) {}
 
-        virtual bool hit(const Ray3D& ray, HitData& hitData, Utils::Range<double> range) override {
-            double denominator = normal.dot(ray.getDirection());
-
-            if (std::fabs(denominator) > 1e-6) {  // Check not parallel
-                double t = (position - ray.getOrigin()).dot(normal) / denominator;
-                if (t >= range.getMin() && t <= range.getMax()) {
-                    hitData.distanceFromOrigin = t;
-                    hitData.position = ray.at(t);
-                    hitData.normal = normal;
-                    hitData.material = _material; // Assume material is a field in HitData
-                    return true;
-                }
-            }
-            return false;
-        }
+        virtual bool hit(const Ray3D& ray, HitData& hitData, Utils::Range<double> range) override;
 
     private:
         Math::Point3D position;
-        Math::Vector3D normal; // Vecteur normal défini et initialisé
-        std::shared_ptr<IMaterial> _material;  // Placeholder for material class
+        Math::Vector3D normal;
+        std::shared_ptr<IMaterial> _material;
     };
 
-} // namespace Rtx
+}
