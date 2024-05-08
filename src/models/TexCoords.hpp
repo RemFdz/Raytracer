fbab5101cf64f3a19d6ee4ef8d0c7aa132eeaf46
RemFdz <remi.fernandez@epitech.eu>
2024-05-08 14:38:44 +0200

    feat: texture interface and class for flatColor

---
+++ src/models/TexCoords.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** raytracer
+** File description:
+** texture coordinates model
+*/
+
+#pragma once
+
+namespace Rtx {
+    struct TexCoords {
+        double u = 0.0;
+        double v = 0.0;
+    };
+}
