d39dd6a647dd0a08f3804c6535ae0f35b72d726c
Romain <romain.collignon@epitech.eu>
2024-05-08 20:48:42 +0200

    feature(Objects): add Object Cone

---
+++ src/objects/shapes/Cone/Cone.cpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2024
+** ray_tracer
+** File description:
+** Cone
+*/
+
+#include "Cone.hpp"
+
+bool Rtx::Cone::hit(const Ray3D& ray, HitData& hitData, Utils::Range<double> range) {
+    const Math::Vector3D& origin = ray.getOrigin();
+    const Math::Vector3D& direction = ray.getDirection().unitVector();
+    Math::Vector3D apexToOrigin = origin - apex;
+
+    double directionDotAxis = direction.dot(axis);
+    Math::Vector3D perpendicular = direction - axis * directionDotAxis;
+    Math::Vector3D p = apexToOrigin - axis * apexToOrigin.dot(axis);
+
+    double a = perpendicular.lengthSquared() - sinAngle * directionDotAxis * directionDotAxis;
+    double b = 2.0 * (perpendicular.dot(p) - sinAngle * directionDotAxis * apexToOrigin.dot(axis));
+    double c = p.lengthSquared() - sinAngle * apexToOrigin.dot(axis) * apexToOrigin.dot(axis);
+
+    double discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+        return false;
+    }
+
+    double sqrtD = sqrt(discriminant);
+    std::array<double, 2> solutions = {(-b - sqrtD) / (2 * a), (-b + sqrtD) / (2 * a)};
+    bool hitSomething = false;
+    double closest_t = range.getMax();
+
+    for (double t : solutions) {
+        if (t >= range.getMin() && t < closest_t) {
+            Math::Point3D point = ray.at(t);
+            double heightAtHit = (point - apex).dot(axis);
+            if (heightAtHit >= 0 && heightAtHit <= height) {
+                closest_t = t;
+                hitSomething = true;
+                hitData.distanceFromOrigin = t;
+                hitData.position = point;
+                hitData.normal = calculateNormal(point, heightAtHit);
+                hitData.material = _material;
+            }
+        }
+    }
+
+    return hitSomething;
+}
---
+++ src/objects/shapes/Cone/Cone.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2024
+** ray_tracer
+** File description:
+** Cone
+*/
+
+#pragma once
+
+#include "../../IObject3D.hpp"
+#include "../../../graphics/ray/Ray3D.hpp"
+#include "../../../math/vector3d/Vector3D.hpp"
+#include "../../IObject3D.hpp"
+#include "../../../graphics/ray/Ray3D.hpp"
+#include "../../../math/vector3d/Vector3D.hpp"
+#include <cmath>
+#include <array>
+#include <memory>
+
+namespace Rtx {
+
+    class Cone : public IObject3D {
+        public:
+            Cone(const Math::Vector3D& apex, const Math::Vector3D& axis, double angle, double height, const std::shared_ptr<IMaterial>& material)
+            : apex(apex), axis(axis.unitVector()), angle(angle), height(height), _material(material) {
+                sinAngle = std::sin(angle);
+                sinAngle *= sinAngle;
+            }
+
+            virtual bool hit(const Ray3D& ray, HitData& hitData, Utils::Range<double> range) override;
+
+        private:
+            Math::Point3D apex;
+            Math::Vector3D axis;
+            double angle;
+            double sinAngle; // Stored as sin^2(angle)
+            double height;
+            std::shared_ptr<IMaterial> _material;
+
+            Math::Vector3D calculateNormal(const Math::Point3D& point, double heightAtHit) const {
+                Math::Vector3D radialVector = (point - (apex + axis * heightAtHit)).unitVector();
+                return radialVector.cross(axis).cross(radialVector).unitVector();
+            }
+    };
+}
+
