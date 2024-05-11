c118a701dd28ade015bf935035b7d46f7eca833f
Romain <romain.collignon@epitech.eu>
2024-05-11 13:08:31 +0200

    feature(Objects): add Object Cube (not parse)

---
+++ src/objects/shapes/Cube/Cube.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2024
+** ray_tracer
+** File description:
+** Cube
+*/
+
+#pragma once
+
+#include "../../IObject3D.hpp"
+#include "../../../graphics/ray/Ray3D.hpp"
+#include "../../../math/vector3d/Vector3D.hpp"
+
+namespace Rtx {
+
+    class Cube : public IObject3D {
+    public:
+        Cube(const Math::Point3D& center, double edgeLength, const std::shared_ptr<IMaterial> &material)
+            : center(center), edgeLength(edgeLength), _material(material) {}
+
+        virtual bool hit(const Ray3D& ray, HitData& hitData, Utils::Range<double> range) override;
+
+    private:
+        Math::Point3D center;
+        double edgeLength;
+        std::shared_ptr<IMaterial> _material;
+
+        bool nearEqual(double a, double b, double epsilon = 1e-10) {
+            return std::abs(a - b) < epsilon;
+        }
+    };
+
+}
