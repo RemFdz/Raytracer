62b36473ac40e5857a1060333e861a8516e9c81e
RemFdz <remi.fernandez@epitech.eu>
2024-05-12 17:50:07 +0200

    doc: documenting randomizer and range classes

--- src/utils/Randomizer.hpp
+++ src/utils/Randomizer.hpp
@@ -13,12 +13,27 @@
 
 namespace Utils {
     template<typename T>
+    /**
+    * @brief Class to generate random numbers
+    * @tparam T type of the random number
+    * **/
     class Randomizer {
     public:
+        /**
+        * @brief Generate a random number between 0 and 1
+        * @return the random number
+        **/
         static inline double getRandom() {
             static std::mt19937 generator(std::random_device{}());
             return std::uniform_real_distribution<T>(0.0, 1.0)(generator);
         }
+
+        /**
+        * @brief Generate a random number between min and max
+        * @param min the minimum value
+        * @param max the maximum value
+        * @return the random number
+        **/
         static inline double getRandom(T min, T max) { return min + (max - min) * getRandom(); }
     };
 }
--- src/utils/Range.hpp
+++ src/utils/Range.hpp
@@ -11,19 +11,42 @@
 #define infinity std::numeric_limits<double>::infinity()
 
 namespace Utils {
+    /**
+    * @brief Class to define an range interval
+    * @tparam T type of the interval
+    * @details
+    * This class is used to define an interval between two values
+    * It can be used to check if a value is in the interval, or to clamp a value to the interval
+    **/
     template<typename T>
     class Range {
     public:
         Range(T min, T max): _min(min), _max(max) {};
         ~Range() = default;
 
+        /**
+        * @brief Clamp a value to the interval
+        * @param value the value to clamp
+        * @return the clamped value
+        **/
         inline T clamp(T value) const {
             if (value < _min) return _min;
             if (value > _max) return _max;
             return value;
         }
 
+        /**
+        * @brief Check if a value is in the interval or equal interval limits
+        * @param value the value to check
+        * @return true if the value is in the interval, false otherwise
+        **/
         inline bool contains(T value) { return _min <= value && value <= _max; }
+
+        /**
+        * @brief Check if a value is strictly in the interval
+        * @param value the value to check
+        * @return true if the value is strictly in the interval, false otherwise
+        **/
         inline bool surrounds(T value) { return _min < value && value < _max; }
 
         inline T getMin() const { return _min; }
@@ -31,9 +54,6 @@ namespace Utils {
 
         inline void setMin(T min) { _min = min; }
         inline void setMax(T max) { _max = max; }
-
-        static inline Range empty() { return Range(-infinity, infinity); }
-        static inline Range universe() { return Range(-infinity, infinity); }
     private:
         T _min;
         T _max;
